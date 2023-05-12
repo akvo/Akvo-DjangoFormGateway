7578871444d70ca1ba40d52a48237cadd2a49fca
dedenbangkit <mail@dedenbangkit.com>
2023-05-12 07:16:50 +0700

    [#1] Coveralls CI setup

--- dev/backend/test.sh
+++ dev/backend/test.sh
@@ -6,4 +6,15 @@ pip -q install --upgrade pip
 pip -q install --cache-dir=.pip -r requirements.txt
 pip check
 
-python manage.py test
+echo "Running tests"
+COVERAGE_PROCESS_START=./.coveragerc \
+  coverage run --parallel-mode --concurrency=multiprocessing --rcfile=./.coveragerc \
+  manage.py test --shuffle --parallel 4 --verbosity=3
+
+echo "Coverage"
+coverage combine --rcfile=./.coveragerc
+coverage report -m --rcfile=./.coveragerc
+
+if [[ -n "${COVERALLS_REPO_TOKEN:-}" ]] ; then
+  coveralls
+fi
