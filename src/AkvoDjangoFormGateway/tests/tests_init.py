b122940061060cffcdf5036683e6c433c613ba59
dedenbangkit <mail@dedenbangkit.com>
2023-05-12 07:40:29 +0700

    [#1] Modify workflow

--- src/AkvoDjangoFormGateway/tests/tests_init.py
+++ src/AkvoDjangoFormGateway/tests/tests_init.py
@@ -1,5 +1,6 @@
 from django.test import TestCase, Client
-from django.conf import settings
+
+# from django.conf import settings
 
 
 class CheckInitialEndpointTestCase(TestCase):
@@ -9,7 +10,10 @@ class CheckInitialEndpointTestCase(TestCase):
         # Allow GET
         response = client.get("/api/gateway/check/")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json(), {"message": "check"})
+        # self.assertEqual(
+        #     response.json(), {"message": settings.TWILIO_ACCOUNT_SID}
+        # )
+        self.assertEqual(response.json(), {"message": "test"})
 
         # Allow GET
         response = client.post("/api/gateway/check/")
