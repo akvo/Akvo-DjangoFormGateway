382387fab9a903b76712c714dc8e7d17d38f9e4c
ifirmawan <firmawaneiwan@gmail.com>
2023-05-15 19:47:20 +0700

    [#2] Add rest_framework router in urls

--- src/AkvoDjangoFormGateway/urls.py
+++ src/AkvoDjangoFormGateway/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
-from .views import check_view
+from django.urls import path, include
+from .views import AkvoFormViewSet, CheckView
 
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'forms', AkvoFormViewSet, basename='forms')
 urlpatterns = [
-    path('check/', check_view),
+    path('', include(router.urls)),
+    path('check/', CheckView.as_view()),
 ]
