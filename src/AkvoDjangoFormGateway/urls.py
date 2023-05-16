5fbb099c054bbd5a93e41d8d1abc23d8ce23e5bb
ifirmawan <firmawaneiwan@gmail.com>
2023-05-16 22:17:42 +0700

    [#4] Add router register TwilioViewset

--- src/AkvoDjangoFormGateway/urls.py
+++ src/AkvoDjangoFormGateway/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
-from .views import AkvoFormViewSet, CheckView
+from .views import AkvoFormViewSet, CheckView, TwilioViewSet
 
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'forms', AkvoFormViewSet, basename='forms')
+router.register(r'twilio', TwilioViewSet, basename='twilio')
 urlpatterns = [
     path('', include(router.urls)),
     path('check/', CheckView.as_view()),
