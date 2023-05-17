8c5ced53bd6de126ffd79803c7ffd6ac714b9aed
ifirmawan <firmawaneiwan@gmail.com>
2023-05-17 11:42:51 +0700

    [#4] Add geolocation and image validation

--- src/AkvoDjangoFormGateway/utils/validation.py
+++ src/AkvoDjangoFormGateway/utils/validation.py
@@ -1,4 +1,5 @@
 import re
+import json
 from datetime import datetime
 
 
@@ -15,10 +16,27 @@ def is_valid_date(input: str, date_format: str):
 
 
 def is_date(input: str) -> bool:
-    pattern = r'^\d{4}-\d{2}-\d{2}$'
+    pattern = r"^\d{4}-\d{2}-\d{2}$"
     return bool(re.match(pattern, input))
 
 
 def is_alphanumeric(input: str) -> bool:
-    pattern = r'^[A-Za-z0-9]+$'
+    pattern = r"^[A-Za-z0-9]+$"
     return bool(re.match(pattern, input))
+
+
+def is_valid_geolocation(json_string: str):
+    try:
+        data = json.loads(json_string)
+        if isinstance(data, list) and len(data) == 2:
+            lat, lng = data
+            if isinstance(lat, (int, float)) and isinstance(lng, (int, float)):
+                return True
+    except (json.JSONDecodeError, ValueError):
+        pass
+    return False
+
+
+def is_image_string(image_string: str) -> bool:
+    pattern = r"^data:image/(png|jpeg|jpg);base64,"
+    return bool(re.match(pattern, image_string))
