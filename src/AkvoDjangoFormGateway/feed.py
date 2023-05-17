43874423b78e242af37bf4c3cef1a9e7e4886e58
ifirmawan <firmawaneiwan@gmail.com>
2023-05-17 11:43:57 +0700

    [#4] Update last question and add set as completed

--- src/AkvoDjangoFormGateway/feed.py
+++ src/AkvoDjangoFormGateway/feed.py
@@ -6,7 +6,13 @@ from .models import (
     AkvoGatewayQuestion as Questions,
 )
 from .constants import QuestionTypes, StatusTypes
-from .utils.validation import is_number, is_alphanumeric, is_date
+from .utils.validation import (
+    is_number,
+    is_alphanumeric,
+    is_date,
+    is_valid_geolocation,
+    is_image_string,
+)
 
 
 class Feed:
@@ -14,15 +20,15 @@ class Feed:
         return super().__new__(cls)
 
     def __init__(self):
-        self.welcome = ['hi', 'hello', 'info']
+        self.welcome = ["hi", "hello", "info"]
 
     def get_init_survey_session(self, text: str):
         init = False
         form_id = None
-        if '#' in text:
+        if "#" in text:
             info = str(text).split("#")
             form_id = info[1]
-            init = len(info) == 2 and 'READY' in text
+            init = len(info) == 2 and "READY" in text
         return init, form_id
 
     def get_form(self, form_id: int = None, data: FormData = None) -> Forms:
@@ -34,9 +40,16 @@ class Feed:
         return survey
 
     def get_last_question(self, data: FormData) -> Questions:
+        if data.form.ag_form_questions.count() == data.ag_data_answer.count():
+            # Return null if all questions are answered
+            return None
         aws = data.ag_data_answer.all()
         ids = [aw.question.id for aw in aws]
-        qs = data.form.ag_form_questions.exclude(id__in=ids).first()
+        qs = (
+            data.form.ag_form_questions.exclude(id__in=ids)
+            .order_by("order")
+            .first()
+        )
         return qs
 
     def get_question(
@@ -59,7 +72,7 @@ class Feed:
 
     def get_list_form(self) -> str:
         forms = Forms.objects.all()
-        msg = "Welcome to Akvo Flow Survey\n\n."
+        msg = "Welcome to Akvo Survey\n\n."
         msg += "*Please select the form below:*\n"
         for f in forms:
             msg += f"- #{f.id} | {f.name}\n"
@@ -79,6 +92,10 @@ class Feed:
             is_valid = is_number(input=text)
         if question.type == QuestionTypes.date:
             is_valid = is_date(input=text)
+        if question.type == QuestionTypes.geo:
+            is_valid = is_valid_geolocation(json_string=text)
+        if question.type == QuestionTypes.photo:
+            is_valid = is_image_string(image_string=text)
         return is_valid
 
     def insert_answer(self, text: str, question: Questions, data: FormData):
@@ -89,11 +106,11 @@ class Feed:
             value = text
         if question.type == QuestionTypes.geo:
             options = text
-        if question.type == QuestionTypes.text:
-            name = text
         if question.type == QuestionTypes.date:
             dv = datetime.strptime(text)
             name = dv.strftime("%m/%d/%Y")
+        if not name and not value and not options:
+            name = text
         return Answers.objects.create(
             question=question,
             data=data,
@@ -101,3 +118,7 @@ class Feed:
             value=value,
             options=options,
         )
+
+    def set_as_completed(self, data: FormData) -> None:
+        data.status = StatusTypes.submitted
+        data.save()
