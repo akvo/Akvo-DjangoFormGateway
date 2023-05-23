8a40ce5f90a2a1de04e32a91c024a81124cb68bd
ifirmawan <firmawaneiwan@gmail.com>
2023-05-23 21:05:23 +0700

    [#12] Fix url and del the covered unit test

--- src/AkvoDjangoFormGateway/tests/tests_twilio_instance_endpoint.py
+++ src/AkvoDjangoFormGateway/tests/tests_twilio_instance_endpoint.py
@@ -40,244 +40,34 @@ class TwilioInstanceEndpointTestCase(TestCase):
 
     def test_instance_request(self):
         form_id = 1
-
-        json_form = {"From": f"whatsapp:+{phone_number}"}
+        # Send hi as first message
+        reply_text = "hi"
         response = client.post(
-            f"/api/gateway/twilio/{form_id}?format=json", json_form
+            f"/api/gateway/twilio/{form_id}?format=json",
+            {"From": f"whatsapp:+{phone_number}", "Body": reply_text},
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        # first question shown
+        # No welcome message and select forms
+        self.assertNotEqual(response.json(), feed.get_list_form())
+
+        # Got the first question
         fq = Questions.objects.filter(form=form_id).order_by("order").first()
         self.assertEqual(response.json(), f"{fq.order}. {fq.text}")
 
-        reply_text = "answer first question"
-        json_form = {"Body": reply_text, "From": f"whatsapp:+{phone_number}"}
-
-        response = client.post(f"/api/gateway/twilio/{form_id}", json_form)
+        # Survey session is created
         datapoint = feed.get_draft_datapoint(phone=phone_number)
 
+        # Answer first question
+        reply_text = "test complaint"
+        response = client.post(
+            f"/api/gateway/twilio/{form_id}",
+            {"Body": reply_text, "From": f"whatsapp:+{phone_number}"},
+        )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(
             feed.validate_answer(text=reply_text, question=fq, data=datapoint),
             True,
         )
-        # Photo question
-        print("form", datapoint.form)
+        # Show next question
         question = feed.get_question(form=datapoint.form, data=datapoint)
         self.assertEqual(response.json(), f"{question.order}. {question.text}")
-
-        # Answer wrong photo question no MediaContentType0
-        image = "http://twilio.example/image/caseSensiT1Ve2.png"
-        json_form = {
-            "Body": "",
-            "From": f"whatsapp:+{phone_number}",
-            "MediaUrl0": image,
-        }
-        response = client.post("/api/gateway/twilio/", json_form)
-        serializer = TwilioSerializer(data=json_form)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertFalse(serializer.is_valid())
-        self.assertEqual(
-            response.json(),
-            {
-                "non_field_errors": [
-                    "MediaContentType0 is required when MediaUrl0 is present."
-                ]
-            },
-        )
-
-        image_type = "image/png"
-        json_form = {
-            "Body": "",
-            "From": f"whatsapp:+{phone_number}",
-            "MediaContentType0": image_type,
-        }
-        response = client.post("/api/gateway/twilio/", json_form)
-        serializer = TwilioSerializer(data=json_form)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertFalse(serializer.is_valid())
-        self.assertEqual(
-            response.json(),
-            {
-                "non_field_errors": [
-                    "MediaUrl0 is required when MediaContentType0 is present."
-                ]
-            },
-        )
-
-        # Answer right photo question
-        json_form = {
-            "Body": "",
-            "From": f"whatsapp:+{phone_number}",
-            "MediaContentType0": image_type,
-            "MediaUrl0": image,
-        }
-        response = client.post("/api/gateway/twilio/", json_form)
-        stored_image = Answers.objects.filter(question=question).first().name
-        self.assertEqual(image, stored_image)
-        self.assertNotEqual(image.lower(), stored_image)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(
-            feed.validate_answer(
-                text="",
-                question=question,
-                data=datapoint,
-                image_type=image_type,
-            ),
-            True,
-        )
-
-        # GPS question
-        question = feed.get_question(form=datapoint.form, data=datapoint)
-
-        # Answer Wrong GPS question
-        lat = "-7.1161"
-        json_form = {
-            "Body": "",
-            "From": f"whatsapp:+{phone_number}",
-            "Latitude": lat,
-        }
-        text = feed.get_answer_text(
-            body=None,
-            image_url=None,
-            lat=lat,
-        )
-        response = client.post("/api/gateway/twilio/", json_form)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-
-        lng = "103.11"
-        json_form = {
-            "Body": "",
-            "From": f"whatsapp:+{phone_number}",
-            "Longitude": lng,
-        }
-        text = feed.get_answer_text(
-            body=None,
-            image_url=None,
-            lng=lng,
-        )
-        response = client.post("/api/gateway/twilio/", json_form)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-
-        # Answer GPS question
-        json_form = {
-            "Body": "",
-            "From": f"whatsapp:+{phone_number}",
-            "Latitude": lat,
-            "Longitude": lng,
-        }
-        text = feed.get_answer_text(
-            body=None,
-            image_url=None,
-            lat=lat,
-            lng=lng,
-        )
-        response = client.post("/api/gateway/twilio/", json_form)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(
-            feed.validate_answer(
-                text=text,
-                question=question,
-                data=datapoint,
-            ),
-            True,
-        )
-
-        # Phone question
-        question = feed.get_question(form=datapoint.form, data=datapoint)
-        # Answer Phone question
-        reply_text = "62819111"
-        json_form = {
-            "Body": reply_text,
-            "From": f"whatsapp:+{phone_number}",
-        }
-        response = client.post("/api/gateway/twilio/", json_form)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(
-            feed.validate_answer(
-                text=reply_text,
-                question=question,
-                data=datapoint,
-            ),
-            True,
-        )
-
-        # Single option question
-        question = feed.get_question(form=datapoint.form, data=datapoint)
-        # Answer Single option question
-        reply_text = "OptiOn 2"
-        json_form = {
-            "Body": reply_text,
-            "From": f"whatsapp:+{phone_number}",
-        }
-        response = client.post("/api/gateway/twilio/", json_form)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(
-            feed.validate_answer(
-                text=reply_text,
-                question=question,
-                data=datapoint,
-            ),
-            True,
-        )
-
-        # Multiple option question
-        question = feed.get_question(form=datapoint.form, data=datapoint)
-        # Answer Multiple option question
-        reply_text = "multi 1, MULTI 3"
-        json_form = {
-            "Body": reply_text,
-            "From": f"whatsapp:+{phone_number}",
-        }
-        response = client.post("/api/gateway/twilio/", json_form)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(
-            feed.validate_answer(
-                text=reply_text,
-                question=question,
-                data=datapoint,
-            ),
-            True,
-        )
-        answer = Answers.objects.filter(
-            data=datapoint, question=question
-        ).first()
-        self.assertEqual(answer.options, ["multi 1", "multi 3"])
-
-        # Date question
-        question = feed.get_question(form=datapoint.form, data=datapoint)
-
-        # Answer wrong date question
-        reply_text = "15/12/1999"
-        json_form = {
-            "Body": reply_text,
-            "From": f"whatsapp:+{phone_number}",
-        }
-        response = client.post("/api/gateway/twilio/", json_form)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertFalse(
-            feed.validate_answer(
-                text=reply_text, question=question, data=datapoint
-            )
-        )
-        # Answer date question
-        reply_text = "15-12-1999"
-        json_form = {
-            "Body": reply_text,
-            "From": f"whatsapp:+{phone_number}",
-        }
-        response = client.post("/api/gateway/twilio/", json_form)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(
-            feed.validate_answer(
-                text=reply_text,
-                question=question,
-                data=datapoint,
-            ),
-            True,
-        )
-        answer = Answers.objects.filter(
-            data=datapoint, question=question
-        ).first()
-        self.assertEqual(answer.name, reply_text)
-        self.assertEqual(response.json(), "Thank you!")
